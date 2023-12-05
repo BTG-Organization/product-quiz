82e760892e3b844c3027866628526aeb22727c24
Craig Bauer <cbauer@par-cbauer-lt.btg.local>
2023-12-05 15:46:39 -0500

    sample content for testing loading build on site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import './App.css';
+import "./App.css";
+import horizontal from "./images/horiz.png";
+import vertical from "./images/vert.png";
+import roller from "./images/roller.png";
+import motorized from "./images/motorized.png";
 
 function App() {
   return (
@@ -6,10 +10,10 @@ function App() {
       <h1>Product Quiz</h1>
       <p>Please select the type of blinds you arte interested in purchasing</p>
       <div className="icons">
-        <img alt="horizontal blinds" src="./images/horiz.png" />
-        <img alt="vertical blinds" src="./images/vert.png" />
-        <img alt="roller shades" src="./images/roller.png" />
-        <img alt="motorized blinds" src="./images/motorized.png" />
+        <img alt="horizontal blinds" src={horizontal} />
+        <img alt="vertical blinds" src={vertical} />
+        <img alt="roller shades" src={roller} />
+        <img alt="motorized blinds" src={motorized} />
       </div>
     </div>
   );
