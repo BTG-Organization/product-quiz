8894fe2d5507925c15faf0d7b9f73d389c74926d
Craig Bauer <cbauer@par-cbauer-lt.btg.local>
2023-12-05 11:23:33 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Quiz</h1>
+      <p>Please select the type of blinds you arte interested in purchasing</p>
+      <div className="icons">
+        <img alt="horizontal blinds" src="./images/horiz.png" />
+        <img alt="vertical blinds" src="./images/vert.png" />
+        <img alt="roller shades" src="./images/roller.png" />
+        <img alt="motorized blinds" src="./images/motorized.png" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
